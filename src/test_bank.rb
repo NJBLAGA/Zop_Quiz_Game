b22263c6ff8a270b98f0463e085816f0d75bc30d
NJBLAGA <nathanblaga90@gmail.com>
2020-12-14 16:08:13 +1100

    added more questions

--- src/test_bank.rb
+++ src/test_bank.rb
@@ -4,61 +4,58 @@ require 'colorize'
 require 'artii'
 # -----------------------------------------------------------------------------------------
 # Questions Bank.
-Q1 =<<END
-Which of these words is slang for 'money
-a. Dosh
-b. Mush
-c. Kudos
-d. Hash
-END
-# Answer = a
 
-Q2 =<<END
-In the Walt Disney film of the same, what type of creature is Bambi?
+# Level 1
+# Novice Questions
+Q01 =<<END
+In the Walt Disney film, what type of creature is Bambi?
 a. Deer
 b. Elephant
 c. Mouse
 d. Cat
 END
 # Answer = a
-
-Q3 =<<END
-In which country was the 1992 film 'Strictly Ballroom'set?
+Q02 =<<END
+In which country was the 1992 film ‘Strictly Ballroom’ set?
 a. South Africa
 b. Canada
 c. New Zealand
 d. Australia
 END
-# Answer = a
-
-Q4 =<<END
-In Australia, comradeship as an ideal is know by what name?
+# Answer = d
+Q03 =<<END
+In Australia, comradeship as an ideal is known by what name?
 a. Mateship
-b. Buddy system
+b. Buddy System
 c. Equality
 d. Old school tie
 END
 # Answer = a
-
-Q5 =<<END
-Who is colloquially known as a 'bookie'?
+Q04 =<<END
+Who is colloquially known as a ‘bookie’?
 a. Bookmaker
 b. Bookkeeper
 c. Bookbinder
 d. Bookseller
 END
-# Answer = b
-
-Q6 =<<END
-A huge hit in 1967, what was the name of John Farnham's 'cleaning lady'?
+# Answer = a
+Q05 =<<END
+A huge hit in 1967, what was the name of John Farnham’s ‘cleaning lady’?
 a. Josie
 b. Sadie
 c. Betty
 d. Milly
 END
+# Answer = b
+Q06 =<<END
+Which of these words is slang for 'money’?
+a. Dosh
+b. Mush
+c. Kudos
+d. Hash
+END
 # Answer = a
-
-Q7 =<<END
+Q07 =<<END
 A dangerously soft-hearted person is sometimes described as a bleeding what?
 a. Brain
 b. Kidney
@@ -66,8 +63,7 @@ c. Spleen
 d. Heart
 END
 # Answer = d
-
-Q8 =<<END
+Q08 =<<END
 An Australian invention, the flight memory recorder in an aircraft is better known as the what colour box?
 a. Black
 b. Brown
@@ -75,23 +71,675 @@ c. Yellow
 d. Grey
 END
 # Answer = a
-
-Q9 =<<END
-Probably the most famous poerrait ever painted is Leonardo da Vinci's Mona who?
+Q09 =<<END
+Probably the most famous portrait ever painted is Leonardo da Vinci’s ‘Mona who’?
 a. Priscilla
 b. Stella
 c. Lisa
 d. Sophia
 END
 # Answer = c
-
 Q10 =<<END
 When one is in a hallucinatory state, which animals are said to be pink?
 a. Panthers
 b. Elephants
 c. Aardvarks
-d. Goannas
+d. Goanna
+END
+# Answer = b
+Q11 =<<END
+Which large structure in New South Wales is colloquially known as the coathanger?
+a. Sydney Opera House
+b. Sydney Harbour Bridge
+c. Anzac Bridge
+d. Cahill Expressway
+END
+# Answer = b
+Q12 =<<END
+From which language do we get the word cappuccino?
+a. Italian
+b. French
+c. German
+d. Japanese
+END
+# Answer = a
+Q13 =<<END
+Which of the following pioneered frozen food production?
+a. Clarence Birdseye
+b. Henrik Crowsfoot
+c. Ivor Nestegg
+d. William Duckbill
+END
+# Answer = a
+Q14 =<<END
+Originally made by Bryant and May of Melbourne, Redheads are a famous brand of which product?
+a. Hair dye
+b. Ice blocks
+c. Matches
+d. Toothpicks
+END
+# Answer = c
+Q15 =<<END
+Which Australian animal has a duck-like bill?
+a. Emu
+b. Koala
+c. Platypus
+d. Wombat
+END
+# Answer = c
+
+# Level 1
+# Easy Questions
+
+Q16 =<<END
+What do you do if you ‘grease someone’s palm’?
+a. Tell a fortune
+b. Bribe someone
+c. Give a warning
+d. Shake hands
+END
+# Answer = b
+Q17 =<<END
+What is used to hit the ball in a game of croquet?
+a. Racquet
+b. Bat
+c. Foot
+d. Mallet
+END
+# Answer = d
+Q18 =<<END
+Babe Ruth was famous for playing which sport?
+a. Ice hockey
+b. American Football
+c. Basketball
+d. Baseball
+END
+# Answer = d
+Q19 =<<END
+Which professionals are called ‘realtors’ in the USA?
+a. Undertakers
+b. Lawyers
+c. Estate agents
+d. Bankers
+END
+# Answer = c
+Q20 =<<END
+In which classic TV western series did Lorne Greene play Ben Cartwright?
+a. Bonanza
+b. The High Chaparral
+c. The Virginian
+d. Alias Smith and Jones
+END
+# Answer = a
+Q21 =<<END
+Who played Thelma in the film ‘Thelma and Louise’?
+a. Geena Davis
+b. Annette Bening
+c. Juliette Lewis
+d. Sally Field
+END
+# Answer = a
+Q22 =<<END
+What is a yak?
+a. Sweet Potato
+b. Muslim veil
+c. Boat
+d. Long-haired Tibetan ox
+END
+# Answer = d
+Q23 =<<END
+Featured as a Mad Max movie is Tina Turner’s ‘We Don’t Need Another what’?
+a. Madman
+b. Price rise
+c. Politician
+d. Hero
+END
+# Answer = d
+Q24 =<<END
+To which group of people was Senator Don Chipp referring when he said the Democrats were there ‘to keep the bastards honest’?
+a. Major political parties
+b. Police
+c. Trade unions
+d. Big business
+END
+# Answer = a
+Q25 =<<END
+Who invented psychoanalysis?
+a. Lucien Freud
+b. Clement Freud
+c. Emma Freud
+d. Sigmund Freud
+END
+# Answer = d
+Q26 =<<END
+In traditional Aboriginal ritual practice, to point the bone at another is to intend what result?
+a. Good fortune
+b. Fertility
+c. Recovery from illness
+d. Death
+END
+# Answer = d
+Q27 =<<END
+In pre-decimal currency, a shilling was known by what boy’s name?
+a. Tom
+b. Dick
+c. Harry
+d. Bob
+END
+# Answer = d
+Q28 =<<END
+In the USA, what are Yellowstone and Yosemite?
+a. Great Lakes
+b. Ivy League Colleges
+c. New York Boroughs
+d. National Parks
+END
+# Answer = d
+Q29 =<<END
+A short, light surfboard usually ridden lying down, is a what board?
+a. Boogie
+b. Disco
+c. Woogie
+d. Boofy
+END
+# Answer = a
+Q30 =<<END
+At what distance is Australian swimmer Kieren Perkins an Olympic gold medallist?
+a. 100 metres
+b. 200 metres
+c. 400 metres
+d. 1500 metres
+END
+# Answer = d
+
+# Level 2
+# Medium Questions
+Q31 =<<END
+What is the correct term for the rotating part of a windmill?
+a. Sails
+b. Propellers
+c. Spinners
+d. Wings
+END
+# Answer = a
+Q32 =<<END
+What is the name for a short pithy saying such as ‘In for a penny, in for a pound’?
+a. Simile
+b. Epithet
+c. Proverb
+d. Analogy
+END
+# Answer = c
+Q33 =<<END
+Which emperor built a famous wall to limit incursions of Northern tribes into Roman England?
+a. Hadrian
+b. Julius Caesar
+c. Nero
+d. Claudius
+END
+# Answer = a
+Q34 =<<END
+In which country did the Impressionist art movement begin?
+a. Great Britain
+b. Italy
+c. France
+d. The Netherlands
+END
+# Answer = c
+Q35 =<<END
+Who assassinated Senator Robert Kennedy in 1968?
+a. Sirhan Sirhan
+b. Lee Harvey Oswald
+c. James Earl Ray
+d. John Wilkes Booth
+END
+# Answer = a
+Q36 =<<END
+What is the name for a word made up from initials, such as ‘radar’ or ‘scuba’?
+a. Antonym
+b. Anagram
+c. Acrostic
+d. Acronym 
+END
+# Answer = d
+Q37 =<<END
+What is the chemical symbol for lead?
+a. K
+b. Hg
+c. L
+d. Pb
+END
+# Answer = d
+Q38 =<<END
+Jacob’s Creek is a famous wine-growing location in which valley?
+a. Hunter
+b. Barossa
+c. Clare
+d. Yarra
+END
+# Answer = b
+Q39 =<<END
+What is the cube of the number eight?
+a. 512
+b. 64
+c. 216
+d. 16
+END
+# Answer = a
+Q40 =<<END
+Which classic television series starred Jeremy Irons and Anthony Andrews?
+a. Jewel In The Crown
+b. The Far Pavilions
+c. Brideshead Revisited
+d. The Forsyte Saga
+END
+# Answer = c
+Q41 =<<END
+What nationality is the Formula One racing driver Mika Hakkinen?
+a. Norwegian
+b. Swedish
+c. Danish
+d. Finnish
+END
+# Answer = d
+Q42 =<<END
+Subject of a criminal libel action was Frank Hardy’s 1950 novel ‘Power Without what’?
+a. Authority
+b. Glory
+c. Happiness
+d. Honour
+END
+# Answer = b
+Q43 =<<END
+Of which country was Nicolae Ceausescu the president?
+a. Bulgaria
+b. Romania
+c. Hungary
+d. East Germany
+END
+# Answer = b
+Q44 =<<END
+Don Dunstan was the Labor premier of which Australian state throughout the 1970s?
+a. South Australia
+b. Tasmania
+c. Victoria
+d. Western Australia
+END
+# Answer = a
+Q45 =<<END
+Used in dentistry, an amalgam is an alloy of which metal with other metals?
+a. Gold
+b. Mercury
+c. Iron
+d. Copper
 END
 # Answer = b
 
+# Level 2
+# Hard Questions
+Q46 =<<END
+Who co-wrote the song ‘Do They Know It’s Christmas’ with Bob Geldof?
+a. David Bowie
+b. Midge Ure
+c. Phil Collins
+d. Paul McCartney
+END
+# Answer = b
+Q47 =<<END
+Which sea laps the east coast of Scotland?
+a. North
+b. Caspian
+c. Baltic
+d. Irish
+END
+# Answer = a
+Q48 =<<END
+Which city replaced Calcutta as India’s capital in 1912?
+a. Delhi
+b. Bombay
+c. Colombo
+d. Madras
+END
+# Answer = a
+Q49 =<<END
+Who invented the spinning jenny?
+a. Richard Arkwright
+b. James Hargreaves
+c. James Watt
+d. Samuel Crompton
+END
+# Answer = b
+Q50 =<<END
+A banshee is a spirit in the folklore of which people?
+a. Basque
+b. Irish
+c. American Indian
+d. Maori
+END
+# Answer = b
+Q51 =<<END
+Which religious movement was founded by Mary Baker Eddy?
+a. Jehovah’s Witnesses
+b. Christian Science
+c. Salvation Army
+d. 7th-Day Adventists
+END
+# Answer = b
+Q52 =<<END
+Which king was killed at the Battle of Hastings?
+a. William
+b. John
+c. Edward
+d. Harold
+END
+# Answer = d
+Q53 =<<END
+Which seat did Pauline Hanson win as an Independent in the 1996 federal election?
+a. Bennelong
+b. Dickson
+c. Oxley
+d. Wills
+END
+# Answer = c
+Q54 =<<END
+What was the name of the world’s first test-tube baby?
+a. Louise Brown
+b. Sarah Evans
+c. Lucy Bryant
+d. Sharon Fuller
+END
+# Answer = a
+Q55 =<<END
+In what artistic occupation is Australian Richard Bonynge internationally renowned?
+a. Conductor
+b. Ballet dancer
+c. Actor
+d. Violinist
+END
+# Answer = a
+Q56 =<<END
+By what name is the baobab tree better known in Australia?
+a. Bottle tree
+b. Banyan
+c. Banyalla
+d. Bangalow
+END
+# Answer = a
+Q57 =<<END
+Which of these great Australia opera singers is known by her original name?
+a. Florence Austral
+b. June Bronhill
+c. Nellie Melba
+d. Joan Sutherland
+END
+# Answer = d
+Q58 =<<END
+In which city is the Hermitage Museum?
+a. Paris
+b. Helsinki
+c. Copenhagen
+d. Saint Petersburg
+END
+# Answer = c
+Q59 =<<END
+Which character in the Sherlock Holmes stories was an Inspector from Scotland Yard?
+a. Lestrade
+b. Hudson
+c. Mycroft
+d. Watson
+END
+# Answer = a
+Q60 =<<END
+How many times was Australia’s Jack Brabham the world Formula One driver’s champion?
+a. Once
+b. Two
+c. Three
+d. Four
+END
+# Answer = c
 
+# Level 3
+# Expert Questions
+Q61 =<<END
+What was the first vineyard established in the Barossa Valley?
+a. Orlando
+b. Penfolds
+c. Seppelt
+d. Yalumba
+END
+# Answer = a
+Q62 =<<END
+What is a trumpet manucode?
+a. Tropical bird
+b. Beetle
+c. Musical instrument
+d. Hearing device
+END
+# Answer = a
+Q63 =<<END
+Who wrote the 1915 classic of polar exploration, ‘The Home of the Blizzard’?
+a. John Davis
+b. Ernest Shackleton
+c. Douglas Mawson
+d. Edgeworth David
+END
+# Answer = c
+Q64 =<<END
+Which poison is obtained from wolf’s-bane?
+a. Strychnine
+b. Aconite
+c. Curare
+d. Cyanide
+END
+# Answer = b
+Q65 =<<END
+Which modern country is the region known in ancient times as Cappadocia?
+a. Greece
+b. Turkey
+c. Bulgaria
+d. Romania
+END
+# Answer = b
+Q66 =<<END
+What structure has two basic types called ‘gravity’ and ‘arch’?
+a. Bridge
+b. Dam
+c. Pier
+d. Road
+END
+# Answer = b
+Q67 =<<END
+Balls Pyramid is a basalt pinnacle located off the east coast of Australia near which island?
+a. Great Sandy
+b. Hook
+c. Lord Howe
+d. Norfolk
+END
+# Answer = c
+Q68 =<<END
+Which Australian athlete won the 80m Hurdles at the 1968 Mexico City Olympics?
+a. Pam Kilborn
+b. Marilyn Black
+c. Maureen Caird
+d. Jennifer Lamy
+END
+# Answer = c
+Q69 =<<END
+Who was Australia’s only Tasmanian-born prime minister of the 20th century?
+a. Arthur Fadden
+b. Joseph Lyons
+c. George Reid
+d. James Scullin
+END
+# Answer = b
+Q70 =<<END
+Which part of an animal’s body is known as the ‘carapace’?
+a. Shell
+b. Snout
+c. Hoof
+d. Tail
+END
+# Answer = a
+Q71 =<<END
+What was ‘Che’ Guevara’s real first name?
+a. Ernesto
+b. Alfredo
+c. Edmundo
+d. Ricardo
+END
+# Answer = a
+Q72 =<<END
+Which of these is a former name for the Russian city of Volgograd?
+a. Leningrad
+b. St Petersburg
+c. Minsk
+d. Stalingrad
+END
+# Answer = d
+Q73 =<<END
+In American football, which team won the most Super Bowls in the 1980s?
+a. Washington Redskins
+b. Chicago Bears
+c. New York Giants
+d. San Francisco 49ers
+END
+# Answer = d
+Q74 =<<END
+‘Varicella’ is the technical term for which disease?
+a. Scarlet fever
+b. Chickenpox
+c. Diabetes
+d. Measles
+END
+# Answer = b
+Q75 =<<END
+What type of living thing is the dugite of south-western Australia?
+a. Lizard
+b. Marsupial
+c. Snake
+d. Tree
+END
+# Answer = c
+
+# Level 3
+# Zop Questions
+Q76 =<<END
+The largest of the great salt lakes of South Australia, Lake Eyre North is connected to Lake Eyre South by which usually dry channel?
+a. Stuart
+b. Torrens
+c. Gawler
+d. Goyder
+END
+# Answer = d
+Q77 =<<END
+Which Australian architect designed the $160 million Yulara tourist resort at Uluru?
+a. Richard Thorp
+b. Daryl Jackson
+c. Glen Murcutt
+d. Philip Cox
+END
+# Answer = d
+Q78 =<<END
+The Four Noble Truths are central to which religion?
+a. Buddhism
+b. Islam
+c. Hinduism
+d. Judaism
+END
+# Answer = a
+Q79 =<<END
+What electoral reform was introduced in South Australia and Victoria in 1856?
+a. Secret ballot
+b. Preferential voting
+c. Compulsory voting
+d. Postal voting
+END
+# Answer = a
+Q80 =<<END
+Which bushranger had the nickname ‘Captain Melville’?
+a. Frederick Ward
+b. Frank McCallum
+c. Martin Cash
+d. John Donahoe
+END
+# Answer = b
+Q81 =<<END
+In which year was the anniversary of Australia’s settlement now called Australia Day, first celebrated as a public holiday?
+a. 1818
+b. 1838
+c. 1888
+d. 1938
+END
+# Answer = b
+Q82 =<<END
+What is the title of the third part of T.S. Eliot’s poem ‘The Waste Land’?
+a. A Game of Chess
+b. Death by Water
+c. What the Thunder Said
+d. The Fire Sermon
+END
+# Answer = d
+Q83 =<<END
+What is ‘phylloxera’?
+a. A lung infection
+b. An insect
+c. A shrub
+d. Green pigment
+END
+# Answer = b
+Q84 =<<END
+In area, which of these Australian national parks is the largest?
+a. Lakefield, Qld
+b. Rudall River, WA
+c. Southwest, Tas
+d. Witjira, SA
+END
+# Answer = b
+Q85 =<<END
+The fall in pitch of a vehicle’s siren as it passes is associated with which scientist?
+a. Corioli
+b. Newton
+c. Doppler
+d. Hooke
+END
+# Answer = c
+Q86 =<<END
+Sir Seretse Khama was the first president of which country?
+a. Botswana
+b. Tanzania
+c. Ghana
+d. Zambia
+END
+# Answer = a
+Q87 =<<END
+Which decimal number is equal to the binary number 10?
+a. Ten
+b. Two
+c. One
+d. Three
+END
+# Answer = b
+Q88 =<<END
+Which star type has the hottest surface temperature?
+a. White
+b. Red
+c. Blue
+d. Yellow
+END
+# Answer = c
+Q89 =<<END
+Which New South Wales governor’s achievements in astronomy brought him greater fame than his military or vice-regal careers?
+a. Bourke
+b. Fitzroy
+c. Darling
+d. Brisbane
+END
+# Answer = d
+Q90 =<<END
+Which of these colours does not appear on the national flag of Lithuania?
+a. Blue
+b. Yellow
+c. Red
+d. Green
+END
+# Answer = a
